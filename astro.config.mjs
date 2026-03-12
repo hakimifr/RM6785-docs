39c6c99e87b1c991a57fbba1f9709a92b26ad14d
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-03-12 23:27:59 +0800

    revamp

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import remarkGithubAlerts from 'remark-github-alerts';
+import "remark-github-alerts/styles/github-colors-light.css";
+import "remark-github-alerts/styles/github-colors-dark-class.css";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkGithubAlerts],
+  },
+  site: 'https://hakimifr.github.io',
+  base: '/RM6785-docs',
+  integrations: [
+    starlight({
+      title: 'RM6785 Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hakimifr/RM6785-docs' }],
+      sidebar: [
+        {
+          label: 'Main',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Main Page', slug: 'index' },
+          ],
+        },
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+        {
+          label: 'Troubleshooting',
+          autogenerate: { directory: 'troubleshooting' },
+        },
+      ],
+    }),
+  ],
+});
